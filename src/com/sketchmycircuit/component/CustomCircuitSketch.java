2b4e019803ded98baac478a0f65113788acf3cbf
nishantmehta.n <nishantmehta.n@gmail.com>
2013-09-30 23:53:19 -0700

    source code by nishant, it contains my current work till erase

--- src/com/sketchmycircuit/component/CustomCircuitSketch.java
+++ src/com/sketchmycircuit/component/CustomCircuitSketch.java
@@ -2,27 +2,53 @@ package com.sketchmycircuit.component;
 
 import java.util.ArrayList;
 
-import com.sketchmycircuit.R;
-
 import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.gesture.Gesture;
 import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
-import android.gesture.Prediction;
+import android.gesture.GestureOverlayView.OnGestureListener;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.GestureStroke;
+import android.gesture.Prediction;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
-public class CustomCircuitSketch extends View implements OnGesturePerformedListener  {
+import com.sketchmycircuit.R;
+
+public class CustomCircuitSketch extends View implements OnGesturePerformedListener,OnGestureListener  {
 
 	private GestureLibrary gestureLib;
 	Context context;
 	public LayoutParams params;
 	public GestureOverlayView gestureOverlayView;
-	
+	SketchView sv;
+	public CustomCircuitSketch(Context context, AttributeSet attrs)
+	{
+		super(context, attrs); 
+		
+		Toast.makeText(context, "HI", Toast.LENGTH_SHORT);
+		gestureOverlayView = new GestureOverlayView(context);
+        gestureOverlayView.setEventsInterceptionEnabled(false);
+        
+        gestureOverlayView.setGestureStrokeType(gestureOverlayView.GESTURE_STROKE_TYPE_MULTIPLE);
+        gestureOverlayView.addOnGesturePerformedListener(this);
+        gestureOverlayView.addOnGestureListener(this);
+        params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+        sv = new SketchView(context);
+        
+        gestureOverlayView.addView(sv);
+        gestureLib = GestureLibraries.fromRawResource(context,R.raw.gestures );
+        if (!gestureLib.load()) {
+            
+          }
+	}
 	public CustomCircuitSketch(Context context)
 	{
 		super(context);
@@ -32,8 +58,9 @@ public class CustomCircuitSketch extends View implements OnGesturePerformedListe
         
         gestureOverlayView.setGestureStrokeType(gestureOverlayView.GESTURE_STROKE_TYPE_MULTIPLE);
         gestureOverlayView.addOnGesturePerformedListener(this);
+        gestureOverlayView.addOnGestureListener(this);
         params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-        SketchView sv = new SketchView(context);
+        sv = new SketchView(context);
         
         gestureOverlayView.addView(sv);
         gestureLib = GestureLibraries.fromRawResource(context,R.raw.gestures );
@@ -47,7 +74,9 @@ public class CustomCircuitSketch extends View implements OnGesturePerformedListe
 		
 		ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
 		
-		
+//		ArrayList<GestureStroke> strokes=gesture.getStrokes();
+//		GestureStroke st= strokes.get(1);
+//		pst.getPath();
 //	    for (Prediction prediction : predictions) {
 //	      if (prediction.score > 2.0) {
 //	        Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT)
@@ -58,9 +87,22 @@ public class CustomCircuitSketch extends View implements OnGesturePerformedListe
 //	    }
 		
 		Prediction bestPrediction= findBestPrediction(predictions);
-		if (bestPrediction.score>1)
+		if (bestPrediction.score>2)
+		{
+		
+		//sv.makeDefaultSketch();
+		if(bestPrediction.name.equalsIgnoreCase("delete"))
+		{
+			ArrayList<GestureStroke> strokes=gesture.getStrokes();
+			GestureStroke st= strokes.get(0);
+			
+			RectF rec =st.boundingBox;
+			Path pt = st.getPath();
+			sv.setErase(rec,pt);
+		}
 		Toast.makeText(context, bestPrediction.name + " " + bestPrediction.score, Toast.LENGTH_SHORT)
         .show();
+		}
 		else
 			Toast.makeText(context, "Sorry no match", Toast.LENGTH_SHORT)
 	        .show();
@@ -73,6 +115,29 @@ public class CustomCircuitSketch extends View implements OnGesturePerformedListe
 			  bestPrediction=prediction;
 		}
 		return bestPrediction;
+	}
+	@Override
+	public void onGesture(GestureOverlayView overlay, MotionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onGestureCancelled(GestureOverlayView overlay, MotionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onGestureEnded(GestureOverlayView overlay, MotionEvent event) {
+		// TODO Auto-generated method stub
+		sv.gestureEnded();
+		
+	}
+	@Override
+	public void onGestureStarted(GestureOverlayView overlay, MotionEvent event) {
+		// TODO Auto-generated method stub
+		sv.gestureStarted();
+//		Toast.makeText(context, "started", Toast.LENGTH_SHORT)
+//        .show();
 	}
 
 }
--- src/com/sketchmycircuit/component/SketchView.java
+++ src/com/sketchmycircuit/component/SketchView.java
@@ -1,88 +1,210 @@
 package com.sketchmycircuit.component;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-
-public class SketchView extends View{
+public class SketchView extends View {
 
 	private Paint brush = new Paint();
-	private  Path path=new Path();
+	private Path path = new Path();
 	public Button btnEraseAll;
 	public LayoutParams params;
-	String TAG="sketch";
-	Boolean mode=false;
-	
+	String TAG = "sketch";
+	Boolean mode = false;
+	Matrix matrix;
+	public float lastX;
+	public float lastY;
+	public boolean gestureStarted;
+	public boolean erase;
+	Path eraseRegion;
+	RectF rec;
+	ArrayList<Path> erasePath = new ArrayList<Path>();
+	ArrayList<RectF> recF= new ArrayList<RectF>();
+	static int eraseCount=0;
+	int firstTime=0;
+	Context context;
+
 	public SketchView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+		this.context=context;
+		this.resetbrush();
+		
+		
+		btnEraseAll = new Button(context);
+		btnEraseAll.setText("erase all");
+		params = new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.WRAP_CONTENT);
+		btnEraseAll.setLayoutParams(params);
+		
+
+	}
+
+	 void resetbrush()
+	{
+		brush.reset();
 		brush.setAntiAlias(true);
 		brush.setColor(Color.GRAY);
 		brush.setStyle(Paint.Style.STROKE);
 		brush.setStrokeJoin(Paint.Join.ROUND);
-		brush.setStrokeWidth(6f);
-		btnEraseAll= new Button(context);
-		btnEraseAll.setText("erase all");
-		params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		btnEraseAll.setLayoutParams(params);
-		btnEraseAll.setOnClickListener(new View.OnClickListener() {
-	            
-	            @Override
-	            public void onClick(View view) {
-	                //reset the path
-	               // path.reset();
-	                mode=true;
-	                //invalidate the view
-	                postInvalidate();
-	                
-	            }
-	        });
+		brush.setStrokeWidth(4f);
+	}
+	public void makeDefaultSketch(float x, float y) {
+//		matrix = new Matrix();
+//		matrix.setScale(1f, 1f);
+//
+//		int w = 1000;
+//		int h = 1000;
+//		path.reset();
+//		path.moveTo(x, y);
+//
+//		path.lineTo(x + 20, y);
+//		path.lineTo(x + 40, y + 25);
+//		path.lineTo(x + 60, y - 25);
+//		path.lineTo(x + 80, y + 25);
+//		path.lineTo(x + 100, y);
+//		path.lineTo(x + 120, y);
+		
+		// path.lineTo(350, 100) ;
+
+//		path.transform(matrix);
+		
+		// *****************code to rotate
+		
+		// Matrix mMatrix = new Matrix();
+		// RectF bounds = new RectF();
+		// path.computeBounds(bounds, true);
+		// mMatrix.postRotate(45,
+		// (bounds.right + bounds.left)/2,
+		// (bounds.bottom + bounds.top)/2);
+		// path.transform(mMatrix);
+//		
+//		postInvalidate();
+
+	}
+
+	public void gestureStarted() {
+		gestureStarted = true;
+	}
+
+	public void gestureEnded() {
+		makeDefaultSketch(lastX, lastY);
+	}
+	public void setErase(RectF rt,Path r)
+	{
+		rec = new RectF();
+		//rec=rt;
+		r.close();
+		erasePath.add(r);
+		recF.add(rt);
+		eraseRegion=r;
+		erase=true;
+		Rect ro = new Rect();
+		rt.round(ro);
+		invalidate(ro);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		// *** code to paste the fair diagram
+//		if (gestureStarted) {
+//
+//			lastX = event.getX();
+//			lastY = event.getY();
+//			gestureStarted = false;
+//		}
+		 //Log.d(TAG,"inside touch");
+		
+		 float pointX = event.getX();
+		 float pointY = event.getY();
+		 //if (mode)
+		 {
+//			 brush.setAlpha(0xFF);
+//			 brush.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+//			brush.setAntiAlias(true);
+		 //brush.setAlpha(0xFF);
+		 //brush.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+		 }
+		 // Checks for the event that occurs
+		 switch (event.getAction()) {
+		 case MotionEvent.ACTION_DOWN:
+		 path.moveTo(pointX, pointY);
+		 
+		 return true;
+		 case MotionEvent.ACTION_MOVE:
+		 path.lineTo(pointX, pointY);
+		 break;
+		 case MotionEvent.ACTION_UP:
+			 invalidate();
+			 break;
+		 default:
+		 return false;
+		 }
+		// Force a view to draw again
+		
+		return false;
 		
 		
 	}
+
+	public void onDrawCustom(Canvas canvas)
+	{
+		
+	}
 	@Override
-    public boolean onTouchEvent(MotionEvent event) {
-		Log.d(TAG,"inside touch");
-		float pointX = event.getX();
-        float pointY = event.getY();
-        if (mode)
-        {
-        	brush.setAlpha(0xFF);
-            //brush.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        }
-        // Checks for the event that occurs
-        switch (event.getAction()) {
-        case MotionEvent.ACTION_DOWN:
-            path.moveTo(pointX, pointY);
-            return true;
-        case MotionEvent.ACTION_MOVE:
-            path.lineTo(pointX, pointY);
-            break;
-        default:
-            return false;
-        }       
-         // Force a view to draw again
-        postInvalidate();
-        return false;
-    
-    }
-	@Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawPath(path, brush);
-        
-    }
-	
-	
-	
+	protected void onDraw(Canvas canvas) {
+			setBackgroundColor(Color.WHITE);
+			
+			Toast.makeText(this.context, "fisttime" +firstTime, Toast.LENGTH_SHORT);
+			Log.d(TAG,"fdhg" +firstTime);
+			//if(firstTime<=4)
+			{
+			Toast.makeText(context, "fisttime" +firstTime, Toast.LENGTH_SHORT);
+			//path.setFillType(Path.FillType.WINDING);
+			//brush.setStyle(Paint.Style.FILL);
+			canvas.drawPath(path, brush);
+			
+			firstTime++;
+			
+			}
+			if(erase)
+			{
+				
+				RectF rect = new RectF();
+				
+				 brush.setColor(Color.WHITE);
+				 brush.setStyle(Paint.Style.FILL);
+				 
+				
+				for(int i =0 ;i<erasePath.size();i++)
+				 {
+					 
+					 canvas.drawRect(recF.get(i), brush);
+					//canvas.drawPath(erasePath.get(i), brush);
+				 }
+				 
+				 erase=false;
+				 this.resetbrush();
+				 //canvas.drawPath(eraseRegion,brush);
+				 
+			}
+			
+		
+	}
 
 }
